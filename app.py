02419381bd801adb1fc301191050cf41e6485812
NNay185 <nayelli.dantas@escola.pr.gov.br>
2025-08-25 09:41:43 -0300

    criei as listas

--- app.py
+++ app.py
@@ -1,1 +1,8 @@
 # Lista de convidados que foram convidados para a festa
+convidados_da_festa = ['Nayelli','Breno','Bia','Djulia','Isa','Gui','Nathi','Dani']
+
+# Dicionário vazio para armazenar o status de presença de cada pessoa 
+status_presenca = {}
+
+# Esta é a lista de pessoas que tentaram entrar na festa
+lista_de_convidados = ['Breno','Bia','Djulia','Isa','Gui']
